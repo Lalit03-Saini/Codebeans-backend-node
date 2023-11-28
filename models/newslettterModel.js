435a4f779b3ea0f9e3c4cbf8e127dbe8acc88051
Lalit03-Saini <lalitdaiya54@gmail.com>
2023-11-28 12:04:58 +0530

    Updating rex and

--- models/newslettterModel.js
+++ models/newslettterModel.js
@@ -1,13 +1,30 @@
+// const mongoose = require("mongoose");
+
+// const newsletterSchema = new mongoose.Schema(
+//     {
+//         email: {
+//             type: email,
+//             required: true,
+//         },
+//     }, { timestamps: true }
+
+// );
+
+// module.exports = mongoose.model("Newslettersub", newsletterSchema)
+
 const mongoose = require("mongoose");
 
 const newsletterSchema = new mongoose.Schema(
     {
         email: {
-            type: email,
+            type: String,
             required: true,
+            unique: true, // Ensures uniqueness of the email field
+            lowercase: true, // Stores email addresses in lowercase
+            match: [/^\S+@\S+\.\S+$/, 'Invalid email format'], // Regex match for email format
         },
-    }, { timestamps: true }
-
+    },
+    { timestamps: true }
 );
 
-module.exports = mongoose.model("Newslettersub", newsletterSchema)
+module.exports = mongoose.model("Newslettersub", newsletterSchema);
