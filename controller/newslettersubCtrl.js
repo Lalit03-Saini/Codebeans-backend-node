df624fae6a313f24abb7e31c7e74879a2a383eed
Lalit Saini <112317285+Lalit03-Saini@users.noreply.github.com>
2023-11-30 15:06:30 +0530

    Update newslettersubCtrl.js

--- controller/newslettersubCtrl.js
+++ controller/newslettersubCtrl.js
@@ -7,7 +7,6 @@ const createNewSub = asyncHandler(async (req, res) => {
         res.status(201).json({
             message: 'Thank you for subscribing to our newsletter!'
         });
-        console.log(newSub)
     } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ error: 'An error occurred while processing your request. Please try again later.' });
