2564a35f0dade43c6c17836c85585e953534a04d
Lalit03-Saini <lalitdaiya54@gmail.com>
2023-11-30 16:39:59 +0530

    testing for giving row data

--- models/newslettterModel.js
+++ models/newslettterModel.js
@@ -1,27 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const newsletterSchema = new mongoose.Schema(
-//     {
-//         subemail: {
-//             type: String,
-//             required: true,
-//             unique: true, // Ensures uniqueness of the email field
-//             lowercase: true, // Stores email addresses in lowercase
-//             match: [/\S+@\S+\.\S+/, 'Invalid email format'], // Regex match for email format
-//             validate: {
-//                 validator: function (v) {
-//                     return /\S+@\S+\.\S+/.test(v); // Custom validator for email format
-//                 },
-//                 message: 'Invalid email format',
-//             },
-//         },
-//     },
-//     { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Newslettersub", newsletterSchema);
-
-
 const mongoose = require("mongoose");
 
 const newsletterSchema = new mongoose.Schema(
@@ -29,15 +5,7 @@ const newsletterSchema = new mongoose.Schema(
         subemail: {
             type: String,
             required: true,
-            unique: true,
             lowercase: true,
-            match: [/\S+@\S+\.\S+/, 'Invalid email format'],
-            validate: {
-                validator: function (v) {
-                    return /\S+@\S+\.\S+/.test(v);
-                },
-                message: 'Invalid email format',
-            },
         },
     },
     { timestamps: true }
