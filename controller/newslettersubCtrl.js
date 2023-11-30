69279c5dda26abc8db38c638322ad1b2c1ec7108
Lalit Saini <112317285+Lalit03-Saini@users.noreply.github.com>
2023-11-30 15:03:19 +0530

    Update newslettersubCtrl.js

--- controller/newslettersubCtrl.js
+++ controller/newslettersubCtrl.js
@@ -1,51 +1,45 @@
 const Newslettersub = require("../models/newslettterModel");
 const asyncHandler = require("express-async-handler");
 
-//-------------Createing New subcriber -----------//
-const createnewsub = asyncHandler(async (req, res) => {
+const createNewSub = asyncHandler(async (req, res) => {
     try {
-        const NewSub = await Newslettersub.create(req.body);
+        const newSub = await Newslettersub.create(req.body);
         res.status(201).json({
             message: 'Thank you for subscribing to our newsletter!'
         });
-        console.log(NewSub)
     } catch (error) {
         console.error('Error creating user:', error);
-
-        res.status(500).json({ message: "An error occurred while processing your request. Please try again later." });
+        res.status(500).json({ error: 'An error occurred while processing your request. Please try again later.' });
     }
 });
 
-//---------Updateing A E-mail of newslettersub-----//
-const Updatenewslettersub = asyncHandler(async (req, res) => {
+const updateNewsletterSub = asyncHandler(async (req, res) => {
     const { id } = req.params;
     try {
-        const updatednewsletterSub = await Newslettersub.findByIdAndUpdate(id, req.body, { new: true });
-        res.status(201).json(updatednewsletterSub);
+        const updatedNewsletterSub = await Newslettersub.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json(updatedNewsletterSub);
     } catch (error) {
         res.status(500).json({ error: `An error occurred: ${error.message}` });
     }
 });
 
-//-------Deleteing a E-mail of newslettersub-------//
-const Deletenewslettersub = asyncHandler(async (req, res) => {
+const deleteNewsletterSub = asyncHandler(async (req, res) => {
     const { id } = req.params;
     try {
-        const deletenewsletterSub = await Newslettersub.findByIdAndDelete(id, req.body, { new: true });
-        res.status(201).json(deletenewsletterSub)
+        await Newslettersub.findByIdAndDelete(id);
+        res.status(204).json();
     } catch (error) {
         res.status(500).json({ error: `An error occurred: ${error.message}` });
     }
 });
 
-//----------Getall E-mail of Newslettersub--------//
-const getallsub = asyncHandler(async (req, res) => {
+const getAllSub = asyncHandler(async (req, res) => {
     try {
-        const GetallSub = await Newslettersub.find()
-        res.status(201).json(GetallSub);
+        const getAllSub = await Newslettersub.find();
+        res.status(200).json(getAllSub);
     } catch (error) {
         res.status(500).json({ error: `An error occurred: ${error.message}` });
     }
 });
 
-module.exports = { createnewsub, Updatenewslettersub, Deletenewslettersub, getallsub }
+module.exports = { createNewSub, updateNewsletterSub, deleteNewsletterSub, getAllSub };
