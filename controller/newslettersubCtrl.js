9f64b6196e355fda2247e864c1bbe270047dd37b
Lalit03-Saini <lalitdaiya54@gmail.com>
2023-11-30 16:21:11 +0530

    changing code for removing error An error occurred: E11000 duplicate key error collection: Codebeans.newslettersubs index: email_1 dup key: { email: null }"

--- controller/newslettersubCtrl.js
+++ controller/newslettersubCtrl.js
@@ -1,7 +1,56 @@
-const Newslettersub = require("../models/newslettterModel");
+// const Newslettersub = require("../models/newslettterModel");
+// const asyncHandler = require("express-async-handler");
+
+// //-------------Createing New subcriber -----------//
+// const createnewsub = asyncHandler(async (req, res) => {
+//     try {
+//         const NewSub = await Newslettersub.create(req.body);
+//         res.status(201).json({
+//             message: 'Thank you for subscribing to our newsletter!'
+//         });
+//     } catch (error) {
+//         res.status(500).json({ error: `An error occurred: ${error.message}` });
+//     }
+// });
+
+// //---------Updateing A E-mail of newslettersub-----//
+
+// const Updatenewslettersub = asyncHandler(async (req, res) => {
+//     const { id } = req.params;
+//     try {
+//         const updatednewsletterSub = await Newslettersub.findByIdAndUpdate(id, req.body, { new: true });
+//         res.status(201).json(updatednewsletterSub);
+//     } catch (error) {
+//         res.status(500).json({ error: `An error occurred: ${error.message}` });
+//     }
+// });
+
+// //-------Deleteing a E-mail of newslettersub-------//
+// const Deletenewslettersub = asyncHandler(async (req, res) => {
+//     const { id } = req.params;
+//     try {
+//         const deletenewsletterSub = await Newslettersub.findByIdAndDelete(id, req.body, { new: true });
+//         res.status(201).json(deletenewsletterSub)
+//     } catch (error) {
+//         res.status(500).json({ error: `An error occurred: ${error.message}` });
+//     }
+// });
+
+// //----------Getall E-mail of Newslettersub--------//
+// const getallsub = asyncHandler(async (req, res) => {
+//     try {
+//         const GetallSub = await Newslettersub.find()
+//         res.status(201).json(GetallSub);
+//     } catch (error) {
+//         res.status(500).json({ error: `An error occurred: ${error.message}` });
+//     }
+// });
+
+// module.exports = { createnewsub, Updatenewslettersub, Deletenewslettersub, getallsub }
+
+const Newslettersub = require("../models/newsletterModel");
 const asyncHandler = require("express-async-handler");
 
-//-------------Createing New subcriber -----------//
 const createnewsub = asyncHandler(async (req, res) => {
     try {
         const NewSub = await Newslettersub.create(req.body);
@@ -9,40 +58,42 @@ const createnewsub = asyncHandler(async (req, res) => {
             message: 'Thank you for subscribing to our newsletter!'
         });
     } catch (error) {
-        res.status(500).json({ error: `An error occurred: ${error.message}` });
+        // Check for duplicate key error and handle it explicitly
+        if (error.code === 11000) {
+            res.status(400).json({ error: 'Email address is already subscribed.' });
+        } else {
+            res.status(500).json({ error: `An error occurred: ${error.message}` });
+        }
     }
 });
 
-//---------Updateing A E-mail of newslettersub-----//
 const Updatenewslettersub = asyncHandler(async (req, res) => {
     const { id } = req.params;
     try {
         const updatednewsletterSub = await Newslettersub.findByIdAndUpdate(id, req.body, { new: true });
-        res.status(201).json(updatednewsletterSub);
+        res.status(200).json(updatednewsletterSub);
     } catch (error) {
         res.status(500).json({ error: `An error occurred: ${error.message}` });
     }
 });
 
-//-------Deleteing a E-mail of newslettersub-------//
 const Deletenewslettersub = asyncHandler(async (req, res) => {
     const { id } = req.params;
     try {
-        const deletenewsletterSub = await Newslettersub.findByIdAndDelete(id, req.body, { new: true });
-        res.status(201).json(deletenewsletterSub)
+        const deletenewsletterSub = await Newslettersub.findByIdAndDelete(id);
+        res.status(204).send();
     } catch (error) {
         res.status(500).json({ error: `An error occurred: ${error.message}` });
     }
 });
 
-//----------Getall E-mail of Newslettersub--------//
 const getallsub = asyncHandler(async (req, res) => {
     try {
-        const GetallSub = await Newslettersub.find()
-        res.status(201).json(GetallSub);
+        const GetallSub = await Newslettersub.find();
+        res.status(200).json(GetallSub);
     } catch (error) {
         res.status(500).json({ error: `An error occurred: ${error.message}` });
     }
 });
 
-module.exports = { createnewsub, Updatenewslettersub, Deletenewslettersub, getallsub }
+module.exports = { createnewsub, Updatenewslettersub, Deletenewslettersub, getallsub };
--- models/newslettterModel.js
+++ models/newslettterModel.js
@@ -1,3 +1,27 @@
+// const mongoose = require("mongoose");
+
+// const newsletterSchema = new mongoose.Schema(
+//     {
+//         subemail: {
+//             type: String,
+//             required: true,
+//             unique: true, // Ensures uniqueness of the email field
+//             lowercase: true, // Stores email addresses in lowercase
+//             match: [/\S+@\S+\.\S+/, 'Invalid email format'], // Regex match for email format
+//             validate: {
+//                 validator: function (v) {
+//                     return /\S+@\S+\.\S+/.test(v); // Custom validator for email format
+//                 },
+//                 message: 'Invalid email format',
+//             },
+//         },
+//     },
+//     { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Newslettersub", newsletterSchema);
+
+
 const mongoose = require("mongoose");
 
 const newsletterSchema = new mongoose.Schema(
@@ -5,12 +29,12 @@ const newsletterSchema = new mongoose.Schema(
         subemail: {
             type: String,
             required: true,
-            unique: true, // Ensures uniqueness of the email field
-            lowercase: true, // Stores email addresses in lowercase
-            match: [/\S+@\S+\.\S+/, 'Invalid email format'], // Regex match for email format
+            unique: true,
+            lowercase: true,
+            match: [/\S+@\S+\.\S+/, 'Invalid email format'],
             validate: {
                 validator: function (v) {
-                    return /\S+@\S+\.\S+/.test(v); // Custom validator for email format
+                    return /\S+@\S+\.\S+/.test(v);
                 },
                 message: 'Invalid email format',
             },
