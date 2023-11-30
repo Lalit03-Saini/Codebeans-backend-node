f441933cdcabb4c85ff7ca783fbf912107ccd8b1
Lalit03-Saini <lalitdaiya54@gmail.com>
2023-11-30 18:49:50 +0530

    adding new field "Working Place"

--- models/hireingModel.js
+++ models/hireingModel.js
@@ -26,6 +26,10 @@ const hiringSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        workingplace: {
+            type: String,
+            required: true,
+        }
 
     },
     { timestamps: true }
